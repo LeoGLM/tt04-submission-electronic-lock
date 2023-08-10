8b7d1fd6e4a15531f690b0889e0d021401fdd690
LeoGLM <141130618+LeoGLM@users.noreply.github.com>
2023-08-10 17:16:11 +0800

    Update tt_um_electronic_lock_LeoGLM.v

--- src/tt_um_electronic_lock_LeoGLM.v
+++ src/tt_um_electronic_lock_LeoGLM.v
@@ -4,8 +4,20 @@ module tt_um_electronic_lock_LeoGLM(
     input  wire       clk,              // clock
     input  wire       rst_n             // reset_n - low to reset
 );
+    
+    wire [3:0] userinput;
+    wire       lock;
+    
     assign userinput = [3:0] ui_in ;     //1111: no input, 1110: set_passcode, 0001-1001: 1-0 ；1101:cancel
-    assign uo_out      =     lock  ;
+    assign uo_out[0] =       lock  ;
+    
+    assign uo_out[1] = 1'b0;
+    assign uo_out[2] = 1'b0;
+    assign uo_out[3] = 1'b0;
+    assign uo_out[4] = 1'b0;
+    assign uo_out[5] = 1'b0;
+    assign uo_out[6] = 1'b0;
+    assign uo_out[7] = 1'b0;
 
     //output [3:0]states,
     //output [2:0]counters
