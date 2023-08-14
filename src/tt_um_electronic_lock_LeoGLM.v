42ea1d6bb70057d5a37866a45b6b58eb03857b76
LeoGLM <141130618+LeoGLM@users.noreply.github.com>
2023-08-14 12:21:21 +0800

    Update tt_um_electronic_lock_LeoGLM.v
    Signed-off-by: LeoGLM <141130618+LeoGLM@users.noreply.github.com>

--- src/tt_um_electronic_lock_LeoGLM.v
+++ src/tt_um_electronic_lock_LeoGLM.v
@@ -6,7 +6,7 @@ module tt_um_electronic_lock_LeoGLM #( parameter MAX_COUNT = 10_000_000 ) (
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // will go high when the design is enabled
     input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset 
+    input  wire       rst_n     // reset_n - low to reset  
 );
     
     wire [3:0] userinput;
